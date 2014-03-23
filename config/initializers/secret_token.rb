9a7317eb682dd0d027aeea9a1fd238038a48f635
Saxon Fletcher <saxon@projuce.com>
2014-03-23 14:42:51 +1000

    Initialize repo yo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Day::Application.config.secret_key_base = 'de99076f1614cb0b997bcef905cb8d9cdd2112b6adfad0e91899102cefe3a0fd8b252e577b2547392774aad12c7c2face097fa14d77d2be9b073df2b5485e986'
---
+++ vendor/bundle/gems/atomic-1.1.16/ext/siteconf20140323-1946-1dp1gk8.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140323-1946-1f7645y"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
