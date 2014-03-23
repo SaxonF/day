9f65952c9f1587301d296108bf8a2eafce476cda
Saxon Fletcher <saxon@projuce.com>
2014-03-23 18:28:07 +1000

    delete task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -59,7 +59,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url }
+      format.html { redirect_to @task.user }
       format.json { head :no_content }
     end
   end
